ff263933609e0bf8d4605d39a74cd7e38e8fe978
Walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2023-02-05 12:26:59 +0900

    0205

---
+++ recommendation/abc181/src/bin/c.rs
@@ -0,0 +1,80 @@
+use std::process::exit;
+
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input!(
+        n : usize,
+        v : [(i32, i32); n],
+
+    );
+
+    for i  in 0..n {
+        for j in 0..i{
+            for k in 0..j{
+                let (x1, y1) = v[i];
+                let (x2, y2) = v[j];
+                let (x3, y3) = v[k];
+                if (x2 - x1) * (y3 - y1) == (x3 - x1) * (y2 - y1){
+                    println!("Yes");
+                    exit(0);
+                }
+
+            }
+        }
+        
+    }
+    println!("No");
+
+}
