e7f1531d03648fbfccec6b61e26336207a3210b3
Walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2023-02-07 12:40:52 +0900

    0207

---
+++ algo/dp/2/7.rs
@@ -0,0 +1,79 @@
+use std::i32::MAX;
+
+
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input!(
+        n : usize,
+        v : [[i32; n]; n],
+    );
+    let mut dp = vec![vec![MAX; n]; n];
+    dp[0][n - 1] = v[0][n - 1];
+
+
+    for i in 0..n {
+        for j in (0..n).rev() {
+            if i != 0 {
+                dp[i][j] = std::cmp::min(dp[i][j], dp[i - 1][j] + v[i][j]);
+            }
+            if j != n - 1 {
+                dp[i][j] = std::cmp::min(dp[i][j], dp[i][j + 1] + v[i][j]);
+            }
+        }
+        
+    }
+    println!("{}", dp[n - 1][0]);
+}
+
